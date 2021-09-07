89b3c09474f489758b947fad45b722bcfe796e35
mubassir3243 <68137780+mubassir3243@users.noreply.github.com>
2021-09-07 14:43:38 +0600

    Add files via upload

---
+++ Front End/carousel.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="#">
+
+<title>Online Movie Tickets Booking System</title>
+
+<link href="https://bootswatch.com/flatly/bootstrap.css" rel="stylesheet">
+<link href="http://netdna.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+<link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+<script src="js/ie-emulation-modes-warning.js"></script>
+
+
+
+
+<link href="css/rotating-card.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/anotherDefault.css" rel="stylesheet">
+</head>
+<body >
+   
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+      <div class="carousel-inner" role="listbox">
+        <div class="item">
+          <img class="first-slide img-responsive" src="images/spdman.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1 class=" alert alert-info">Spiderman: Homecoming<p class="text-warning">Coming this month</p></h1>
+              <p><a class="btn btn-xm btn-success" href="showtimes.html" role="button">Showtime and Details</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item active">
+          <img class="second-slide img-responsive" src="images/thor.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1 class=" alert alert-info">Thor: Ragnorak <p class="text-warning">Showing Now</p></h1>
+              <p><a class="btn btn-xm btn-success" href="showtimes.html" role="button">Showtime and Details</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+  </body>
+  </html>
